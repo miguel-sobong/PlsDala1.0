1b050fc8aceaaafc261925cedd4bf8ee10c033ce
Miggy <migueltsobong@gmail.com>
2018-02-11 23:17:41 +0800

    accidentally deleted a letter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -94,7 +94,7 @@ export class HomePage
               var rating = snapshot.val().rating / snapshot.val().totalrate;
             }
             this.ListOfitems.push({firstname:snapshot.val().firstname, lastname: snapshot.val().lastname, email:snapshot.val().email, 
-              romAddress:res[i].fromAddress, fromDate:res[i].fromDate, toAddress:res[i].toAddress, toDate:res[i].toDate, 
+              fromAddress:res[i].fromAddress, fromDate:res[i].fromDate, toAddress:res[i].toAddress, toDate:res[i].toDate, 
               fromX:res[i].fromX, fromY:res[i].fromY, key:res[i].key, toX:res[i].toX, toY:res[i].toY, userId:res[i].userId, 
               rating: rating});
             this.ListOfitems2nd = this.ListOfitems;
